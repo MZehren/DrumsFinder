41a21a1c2810ae2d58f89200c69e0b28101f8551
Mickael Zehren <mickael.zehren@gmail.com>
2017-07-06 16:57:04 +0200

    Adding a summary.image of the first convolutional layer

--- Backend/src/main.py
+++ Backend/src/main.py
@@ -15,46 +15,96 @@ from scipy.io import wavfile # get the api
 from tensorflow.contrib.metrics.python.metrics.classification import accuracy
 from sympy.physics.units import ten
 from tensorflow.contrib.slim.python.slim.model_analyzer import tensor_description
+from math import sqrt
 
 
+def put_kernels_on_grid (kernel, grid_Y, grid_X, pad = 1):
 
-'''
-create and init weights
-it's better than starting at 0 to prevent a null gradient.
-it's also good to have randomization to prevent symmetry in the network
-And it's good to have a positive initial value to prevent dead ReLu neurons
-'''
+    '''Visualize conv. features as an image (mostly for the 1st layer).
+    Place kernel into a grid, with some paddings between adjacent filters.
+
+    Args:
+      kernel:            tensor of shape [Y, X, NumChannels, NumKernels]
+      (grid_Y, grid_X):  shape of the grid. Require: NumKernels == grid_Y * grid_X
+                           User is responsible of how to break into two multiples.
+      pad:               number of black pixels around each filter (between them)
+
+    Return:
+      Tensor of shape [(Y+2*pad)*grid_Y, (X+2*pad)*grid_X, NumChannels, 1].
+    '''
+
+    x_min = tf.reduce_min(kernel)
+    x_max = tf.reduce_max(kernel)
+
+    kernel1 = (kernel - x_min) / (x_max - x_min)
+
+    # pad X and Y
+    x1 = tf.pad(kernel1, tf.constant( [[pad,pad],[pad, pad],[0,0],[0,0]] ), mode = 'CONSTANT')
+
+    # X and Y dimensions, w.r.t. padding
+    Y = kernel1.get_shape()[0] + 2 * pad
+    X = kernel1.get_shape()[1] + 2 * pad
+
+    channels = kernel1.get_shape()[2]
+
+    # put NumKernels to the 1st dimension
+    x2 = tf.transpose(x1, (3, 0, 1, 2))
+    # organize grid on Y axis
+    x3 = tf.reshape(x2, tf.stack([grid_X, Y * grid_Y, X, channels])) #3
+
+    # switch X and Y axes
+    x4 = tf.transpose(x3, (0, 2, 1, 3))
+    # organize grid on X axis
+    x5 = tf.reshape(x4, tf.stack([1, X * grid_X, Y * grid_Y, channels])) #3
+
+    # back to normal order (not combining with the next step for clarity)
+    x6 = tf.transpose(x5, (2, 1, 3, 0))
+
+    # to tf.image_summary order [batch_size, height, width, channels],
+    #   where in this case batch_size == 1
+    x7 = tf.transpose(x6, (3, 0, 1, 2))
+    # scale to [0, 255] and convert to uint8
+    return tf.image.convert_image_dtype(x7, dtype = tf.uint8) 
+
 def weight_variable(shape):
+    '''
+    create and init weights
+    it's better than starting at 0 to prevent a null gradient.
+    it's also good to have randomization to prevent symmetry in the network
+    And it's good to have a positive initial value to prevent dead ReLu neurons
+    '''
     initial = tf.truncated_normal(shape, stddev=0.1)
     return tf.Variable(initial)
 
-'''
-create and init bias
-'''
 def bias_variable(shape):
+    '''
+    create and init bias
+    '''
     initial = tf.constant(0.1, shape=shape)
     return tf.Variable(initial)
 
-'''
-Create a convolutional layer of stride of 1 and no padding
-which means that the output will be the same size as the input.
-x = input tensor [batch, in_height, in_width, in_channels]
-W = filters [filter_height, filter_width, in_channels, out_channels]
-    in_channels = each color of the image
-    out_channels = activation of each filter
 
-the output is an activation map in 4D as the input
-'''
 def conv2d(x, W):
+    '''
+    Create a convolutional layer of stride of 1 and no padding
+    which means that the output will be the same size as the input.
+    x = input tensor [batch, in_height, in_width, in_channels]
+    W = filters [filter_height, filter_width, in_channels, out_channels]
+        in_channels = each color of the image
+        out_channels = activation of each filter
+    
+    the output is an activation map in 4D as the input
+    '''
     return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
 
-'''
-perform a max pooling
-x is the input tensor of shape [batch, in_height, in_width, in_channels]
-ksize = the size of the window in each dimension
-strides= the stride of the sliding window
-'''
+
 def max_pool_2x2(x):
+    '''
+    perform a max pooling
+    x is the input tensor of shape [batch, in_height, in_width, in_channels]
+    ksize = the size of the window in each dimension
+    strides= the stride of the sliding window
+    '''
     return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
 
 def inference(x, y_, keep_prob):
@@ -68,7 +118,8 @@ def inference(x, y_, keep_prob):
         #the first layer
         h_conv1 = tf.nn.relu(conv2d(x, W_conv1) + b_conv1)
         h_pool1 = max_pool_2x2(h_conv1) #the 2x2 pooling will reduce each the width and the height of the pictures by half
-
+        tf.summary.image("conv1/kernels", put_kernels_on_grid(W_conv1, 4, 8), max_outputs=1)
+        
     with tf.name_scope('conv2'):
         # second layer     
         # we stack layers. This one will have 64 features for each 10*10 patch
